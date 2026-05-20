f8b08762c8b4a5cbf949af2b7c5bc0601ce82ee3
unknown <maria.e.mendes7@edu.senai.br>
2026-05-20 10:25:53 -0300

    feat: adiciona notas e cálculo de média

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main () {
     //Entrada - declaração das variáveis
     string nomes[20];
     int qtdAlunos;
+    float notas[20][5];
+    float media[20];
+    int qtdDisciplinas;
 
     //Processamento
     cout << "=== SISTEMA DE NOTAS V4.0 ===" << endl;
@@ -15,17 +18,43 @@ int main () {
         cin >> qtdAlunos;
     } while (qtdAlunos < 1 || qtdAlunos > 20); 
 
-    cin.ignore();
+    cin.ignore(); 
 
     for (int i = 0; i < qtdAlunos; i++) {
         cout << "Nome do aluno " << i + 1 << ": "; 
         getline(cin, nomes[i]);
     }
 
-    //Saída
-    cout << "\nAlunos cadastrados:" << endl;
+    do {
+        cout << "\nQuantidade de disciplinas (1 a 5): ";
+        cin >> qtdDisciplinas;
+    } while (qtdDisciplinas < 1 || qtdDisciplinas > 5);
+
     for (int i = 0; i < qtdAlunos; i++) {
-        cout << "  " << i + 1 << ". " << nomes[i] << endl;
+        cout << "\nNotas de " << nomes[i] << ":" << endl;
+        float soma = 0;
+        for (int j = 0; j < qtdDisciplinas; j++){
+            do{
+                
+                cout << "Nota da disciplina " << j + 1 << " (0 a 10): ";
+                cin >> notas[i][j];
+            } while (notas[i][j] < 0 || notas[i][j] > 10); 
+            soma += notas[i][j];
+        }
+        media[i] = soma / qtdDisciplinas;
+    }
+
+    //Saída 
+    cout << "\n=====================================" << endl;
+    cout << "        RELATÓRIO DE NOTAS" << endl;
+    cout << "=====================================" << endl;
+    for (int i = 0; i < qtdAlunos; i++) {
+        cout << "Aluno " << i + 1 << ": " << nomes[i] << endl;
+        for (int j = 0; j < qtdDisciplinas; j++) {
+            cout << "  - Disc. " << j + 1 << ": " << notas[i][j] << endl;
+        }
+        cout << "  > Média: " << media[i] << endl;
+        cout << "-------------------------------------" << endl;
     }
 
     return 0; 
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+for (int i = 0; i < qtdAlunos; i++) {
+        cout << "Nome do aluno " << i + 1 << ": "; 
+        getline(cin, nomes[i]);

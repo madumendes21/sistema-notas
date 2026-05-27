0a47b7a183d9816b570001919abf8fe29fc7ecdc
madumendes21 <maria.e.mendes7@edu.senai.br>
2026-05-27 15:25:34 -0300

    feat: adiciona leitura de arquivo

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include <string>
 #include <windows.h>
 #include <fstream>
+#include <clocale>
 
 using namespace std;
 
 int main () {
+    setlocale(LC_ALL, "Portuguese");
 
     //Entrada - declaração das variáveis
     string nomes[20];
@@ -13,11 +15,34 @@ int main () {
     float notas[20][5];
     float media[20];
     int qtdDisciplinas;
+    int opcaoInicial;
 
     //Processamento
 
     //LEITURA DE ALUNOS (commit 1)
     cout << "=== SISTEMA DE NOTAS V4.0 ===" << endl;
+    cout << "1 - Novo relatório" << endl;
+    cout << "2 - Ver relatório salvo" << endl;
+    cout << "Escolha uma opção: " << endl;
+    cin >> opcaoInicial;
+
+    // LEITURA DE ARQUIVO (commit 5)
+    if (opcaoInicial == 2) {
+        ifstream leitura("relatorio.txt");
+        if (leitura.is_open()) {
+            string linha;
+            cout << "\n";
+            while (getline(leitura, linha)) {
+                cout << linha << endl;
+            }
+            leitura.close();
+        } else {
+            cout << "Nenhum relatório encontrado." << endl;
+        }
+        return 0; // ADICIONADO: Encerra o programa aqui para não gerar o loop infinito abaixo!
+    }
+
+
     do {
         cout << "Quantidade de alunos (1 a 20): ";
         cin >> qtdAlunos;
@@ -91,28 +116,30 @@ int main () {
     cout << "Reprovados: " << reprovados << endl;
 
 
-//SALVAR EM ARQUIVO (commit 4)
-ofstream arquivo("relatorio.txt");
+    //SALVAR EM ARQUIVO (commit 4)
+    ofstream arquivo("relatorio.txt");
 
-if (arquivo.is_open()) {
-    arquivo << "=== RELÁTORIO ===" << endl;
-    for (int i = 0; i < qtdAlunos; i++) {
-        arquivo << nomes[i] << " - Média:" << media[i] << " - ";
-        if (media[i] >= 7) {
-            arquivo << "Aprovado" << endl;
-        } else if (media[i] >= 5) {
-            arquivo << "Recuperação" << endl;
-        } else
-         { arquivo << "Reprovado" << endl;
+    if (arquivo.is_open()) {
+        arquivo << "=== RELÁTORIO ===" << endl;
+        for (int i = 0; i < qtdAlunos; i++) {
+            arquivo << nomes[i] << " - Média:" << media[i] << " - ";
+            if (media[i] >= 7) {
+                arquivo << "Aprovado" << endl;
+            } else if (media[i] >= 5) {
+                arquivo << "Recuperação" << endl;
+            } else { 
+                arquivo << "Reprovado" << endl;
+            }
         }
-    }
-    arquivo << "\nResumo: " << aprovados << " aprovados, " << recuperacao << " em recuperação, " << reprovados << " reprovados, " << endl;
+        arquivo << "\nResumo: " << aprovados << " aprovados, " << recuperacao << " em recuperação, " << reprovados << " reprovados, " << endl;
 
-    arquivo.close();
-    cout << "\nRelatório salvo em relatorio.txt" << endl;
-} else {
-    cout << "Erro ao criar arquivo." << endl;
-}
+        arquivo.close();
+        cout << "\nRelatório salvo em relatorio.txt" << endl;
+    } else {
+        cout << "Erro ao criar arquivo." << endl;
+    }
 
     return 0; 
 }
+
+
